ca7620330bff770df1761756c334d1fd2aeedcca
kul1 <1.0@kul.asia>
2020-09-21 20:43:10 -0500

    login not refresh

--- src/components/app.js
+++ src/components/app.js
@@ -1,23 +1,50 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import axios from 'axios';
-import Home from './Home.js';
-import Dashboard from './Dashboard.js';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import Home from "./Home.js";
+import Dashboard from "./Dashboard.js";
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      loggedInStatus: 'NOT_LOGGED_IN',
+      loggedInStatus: "NOT_LOGGED_IN",
       user: {},
     };
 
     this.handleLogin = this.handleLogin.bind(this);
   }
 
+  checkLoginStatus() {
+    axios
+      .get("http://localhost:3001/logged_in", { withCredentials: true })
+      .then((response) => {
+        if (
+          response.data.logged_in &&
+          this.state.loggedInStatus === "NOT_LOGGED_IN"
+        ) {
+          this.setState({
+            loggedInStatus: "LOGGED_IN",
+            user: response.data.user,
+          });
+        } else if (
+          !response.data.logged_in &
+          (this.state.loggedInStatus === "LOGGED_IN")
+        ) {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+            user: {},
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("check login error", error);
+      });
+  }
+
   handleLogin(data) {
     this.setState({
-      loggedInStatus: 'LOGGED_IN',
+      loggedInStatus: "LOGGED_IN",
       user: data.user,
     });
   }
@@ -28,8 +55,8 @@ export default class App extends Component {
           <Switch>
             <Route
               exact
-              path={'/'}
-              render={props => (
+              path={"/"}
+              render={(props) => (
                 <Home
                   {...props}
                   handleLogin={this.handleLogin}
@@ -39,8 +66,8 @@ export default class App extends Component {
             />
             <Route
               exact
-              path={'/dashboard'}
-              render={props => (
+              path={"/dashboard"}
+              render={(props) => (
                 <Dashboard
                   {...props}
                   loggedInStatus={this.state.loggedInStatus}
